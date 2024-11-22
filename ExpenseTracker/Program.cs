07cdd3e92efd3a87419f1b73b3eebfc6f81f15dd
Nika Botchorishvili <nikabotchorishvili@gmail.com>
2024-11-22 12:36:23 +0400

    feat: Setup up basic commands

--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ExpenseTracker.Models;
 
-Console.WriteLine("Hello, World!");
+var isRunning = true;
+
+Console.WriteLine("Welcome to Expense Tracker app");
+Console.WriteLine("Following commands are valid: add, exit, edit, remove, help.");
+while (isRunning)
+
+{
+    var cmd = Console.ReadLine();
+    switch (cmd)
+    {
+        case "add":
+            break;
+        case "edit":
+            break;
+        case "remove":
+            break;
+        case "exit":
+            isRunning = false;
+            break;
+        case "help":
+            Console.WriteLine("Commands: add, exit, edit, remove, help.");
+            break;
+        default:
+            Console.WriteLine("Invalid command. Valid commands are: add, edit, remove, help and exit.");
+            break;
+    }
+}
